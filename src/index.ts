14678e0eba6d58a4273928423cfc4f1ee14340dd
송재진 <wowlxx28@gmail.com>
2025-09-12 00:10:23 +0900

    feat: deploy 설정

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node --max-old-space-size=4096
+
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 

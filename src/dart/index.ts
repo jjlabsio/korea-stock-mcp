905703b6cf8dd8b2f2a69d9857e0f86ce23c6aee
송재진 <wowlxx28@gmail.com>
2025-09-12 10:14:17 +0900

    refactor: dart barrel pattern

---
+++ src/dart/index.ts
@@ -0,0 +1,17 @@
+import { getCorpCode, getCorpCodeSchema } from "./get-corp-code.js";
+export { getCorpCode, getCorpCodeSchema };
+
+import {
+  getDisclosureList,
+  getDisclosureListSchema,
+} from "./get-disclosure-list.js";
+export { getDisclosureList, getDisclosureListSchema };
+
+import { getDisclosure, getDisclosureSchema } from "./get-disclosure.js";
+export { getDisclosure, getDisclosureSchema };
+
+import {
+  getFinancialStatement,
+  getFinancialStatementSchema,
+} from "./get-financial-statement.js";
+export { getFinancialStatement, getFinancialStatementSchema };
--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,7 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 
-import {
-  getFinancialStatement,
-  getFinancialStatementSchema,
-} from "./dart/get-financial-statement.js";
-import { getDisclosure, getDisclosureSchema } from "./dart/get-disclosure.js";
-import {
-  getDisclosureList,
-  getDisclosureListSchema,
-} from "./dart/get-disclosure-list.js";
-import { getCorpCode, getCorpCodeSchema } from "./dart/get-corp-code.js";
+import * as dart from "./dart/index.js";
 import * as krx from "./krx/index.js";
 import * as common from "./common/index.js";
 
@@ -29,15 +20,15 @@ const server = new McpServer({
  */
 
 server.tool(
-  "get_disclosure_list",
+  "get_corp_code",
   `
-  공시검색: 공시 유형별, 회사별, 날짜별 등 여러가지 조건으로 공시보고서 검색기능을 제공합니다.
-  최근 공시를 검색할때는 bgn_de를 반드시 지정하세요.
+  고유번호: DART에 등록되어있는 공시대상회사의 고유번호, 회사명, 종목코드, 최근변경일자 제공합니다.
+  이름이 일치하는 경우 모든 항목을 반환합니다.
   `,
-  getDisclosureListSchema.shape,
+  dart.getCorpCodeSchema.shape,
   async (params) => {
-    const args = getDisclosureListSchema.parse(params);
-    const response = await getDisclosureList(args);
+    const args = dart.getCorpCodeSchema.parse(params);
+    const response = await dart.getCorpCode(args);
 
     return {
       content: [{ type: "text", text: JSON.stringify(response) }],
@@ -46,15 +37,15 @@ server.tool(
 );
 
 server.tool(
-  "get_corp_code",
+  "get_disclosure_list",
   `
-  고유번호: DART에 등록되어있는 공시대상회사의 고유번호, 회사명, 종목코드, 최근변경일자 제공합니다.
-  이름이 일치하는 경우 모든 항목을 반환합니다.
+  공시검색: 공시 유형별, 회사별, 날짜별 등 여러가지 조건으로 공시보고서 검색기능을 제공합니다.
+  최근 공시를 검색할때는 bgn_de를 반드시 지정하세요.
   `,
-  getCorpCodeSchema.shape,
+  dart.getDisclosureListSchema.shape,
   async (params) => {
-    const args = getCorpCodeSchema.parse(params);
-    const response = await getCorpCode(args);
+    const args = dart.getDisclosureListSchema.parse(params);
+    const response = await dart.getDisclosureList(args);
 
     return {
       content: [{ type: "text", text: JSON.stringify(response) }],
@@ -65,10 +56,10 @@ server.tool(
 server.tool(
   "get_disclosure",
   "DART API를 통해 공시보고서 원본파일을 파싱해 가져옵니다.",
-  getDisclosureSchema.shape,
+  dart.getDisclosureSchema.shape,
   async (params) => {
-    const args = getDisclosureSchema.parse(params);
-    const response = await getDisclosure(args);
+    const args = dart.getDisclosureSchema.parse(params);
+    const response = await dart.getDisclosure(args);
 
     return {
       content: [{ type: "text", text: JSON.stringify(response) }],
@@ -79,10 +70,10 @@ server.tool(
 server.tool(
   "get_financial_statement",
   "재무제표: 상장법인(유가증권, 코스닥) 및 주요 비상장법인(사업보고서 제출대상 & IFRS 적용)이 제출한 정기보고서 내에 XBRL재무제표의 모든계정과목을 제공합니다.",
-  getFinancialStatementSchema.shape,
+  dart.getFinancialStatementSchema.shape,
   async (params) => {
-    const args = getFinancialStatementSchema.parse(params);
-    const response = await getFinancialStatement(args);
+    const args = dart.getFinancialStatementSchema.parse(params);
+    const response = await dart.getFinancialStatement(args);
 
     return {
       content: [{ type: "text", text: JSON.stringify(response) }],
